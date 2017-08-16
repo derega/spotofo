3b5920db2aef48578379443636d76df1ea95b351
Ilkka Hakkari <ilkka@haltu.fi>
2017-08-16 18:59:12 +0300

    Spotify user authorization views

--- spotofoweb/urls.py
+++ spotofoweb/urls.py
@@ -2,9 +2,12 @@
 from django.conf.urls import url
 from django.contrib import admin
 from spotofoweb.views import CurrentlyPlayingView
+from spotofoweb.views import AuthorizeUserView, AuthorizeResponseView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', CurrentlyPlayingView.as_view()),
+    url(r'^authorize$', AuthorizeUserView.as_view()),
+    url(r'^authorize/response$', AuthorizeResponseView.as_view()),
 ]
 
--- spotofoweb/views.py
+++ spotofoweb/views.py
@@ -1,6 +1,8 @@
 
 from django.core.cache import cache
-from django.views.generic import TemplateView
+from django import forms
+from django.http import HttpResponseRedirect
+from django.views.generic import View, FormView, TemplateView
 import spotofo
 
 
@@ -13,4 +15,30 @@ class CurrentlyPlayingView(TemplateView):
     cp = list(spotofo.get_currently_playing_trackinfo(users))
     context['currently_playing'] = cp
     return context
+
+
+class UsernameForm(forms.Form):
+  username = forms.CharField(label='username', max_length=2048)
+
+
+class AuthorizeUserView(FormView):
+  template_name = 'spotofoweb/begin_authorize_user.html'
+  form_class = UsernameForm
+
+  def form_valid(self, form):
+    username = form.cleaned_data['username']
+    sp_oauth = spotofo.oauth_client()
+    url = sp_oauth.get_authorize_url(state=username)
+    self.success_url = url
+    return super(FormView, self).form_valid(form)
+
+
+class AuthorizeResponseView(View):
+  def get(self, request, *args, **kwargs):
+    code = request.GET.get('code', None)
+    username = request.GET.get('state', None)
+    sp_oauth = spotofo.oauth_client()
+    token_info = sp_oauth.get_access_token(code)
+    spotofo.save_token_info(username, token_info)
+    return HttpResponseRedirect('/')
 
